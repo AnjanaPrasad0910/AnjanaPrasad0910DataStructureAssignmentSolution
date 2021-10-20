62a30fc6865d53f1bea9f50fe2e758f2f98f0709
Anjana Prasad Nair <Anjana Prasad Nair@DESKTOP-HTKRUKG>
2021-10-20 16:31:27 +0530

    First Commit

--- Skyscraper&Skewed_Tree_Project/src/com/gl/dsaga/skewedtree/driver/SkewedTreeConversion.java
+++ Skyscraper&Skewed_Tree_Project/src/com/gl/dsaga/skewedtree/driver/SkewedTreeConversion.java
@@ -19,14 +19,14 @@ public class SkewedTreeConversion {
 	static Node prevNode = null;
 	static Node headNode = null;
 	
-	static void flattenBTToSkewed(Node root)
+	static void convertBTToSkewed(Node root)
 	{
 	
 		if(root == null)
 		{
 			return;
 		}
-		flattenBTToSkewed(root.left);
+		convertBTToSkewed(root.left);
 		Node rightNode = root.right;
 	
 		if(headNode == null)
@@ -41,18 +41,18 @@ public class SkewedTreeConversion {
 			root.left = null;
 			prevNode = root;
 		}
-		flattenBTToSkewed(rightNode);
+		convertBTToSkewed(rightNode);
 
 	}
 
-	static void traverseRightSkewed(Node root)
+	static void traverseRightSkewedTree(Node root)
 	{
 		if(root == null)
 		{
 			return;
 		}
 		System.out.print(root.data + " ");
-		traverseRightSkewed(root.right);	
+		traverseRightSkewedTree(root.right);	
 	}
 	
 	//Drivers Code
@@ -65,8 +65,8 @@ public class SkewedTreeConversion {
 		 tree.node.left.left = new Node(10);
 		 tree.node.right.left= new Node(55);
 
-		flattenBTToSkewed(node);
-		traverseRightSkewed(headNode);
+		convertBTToSkewed(node);
+		traverseRightSkewedTree(headNode);
 		
 
 	}
